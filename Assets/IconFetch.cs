52fc7b7152a9dc294703b2750f02a5a8c4509498
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2025-01-12 00:30:35 -0500

    Change concurrency model
    This avoids a race condition, since Unity disallows waiting on a
    coroutine multiple times at once.

--- Assets/IconFetch.cs
+++ Assets/IconFetch.cs
@@ -12,64 +12,74 @@ public class IconFetch : MonoBehaviour
 
     public static IconFetch Instance { get; private set; }
 
-    private Coroutine _fetchRoutineRepo;
-
-    private Coroutine _fetchRoutineIconSprite;
-
     private KtaneModule[] _modules;
 
     private Texture2D _iconSprite;
 
-    private bool _isError = false;
+    private bool _isError = false, _fetchRepoDone = false, _fetchIconSpriteDone = false;
 
     private void Awake()
     {
         // set static instance
         Instance = this;
         // start fetching repo data and iconsprite
-        _fetchRoutineRepo = StartCoroutine(FetchRepo());
-        _fetchRoutineIconSprite = StartCoroutine(FetchIconSprite());
+        StartCoroutine(FetchRepo());
+        StartCoroutine(FetchIconSprite());
     }
 
     private IEnumerator FetchRepo()
     {
-        Log("Fetching repository data");
-
-        // fetch json data
-        using (var req = UnityWebRequest.Get(string.Format("{0}/json/raw", RepositoryHost)))
+        try
         {
-            yield return req.SendWebRequest();
-            if (req.isHttpError || req.isNetworkError)
-            {
-                _isError = true;
-                Log("Failed to get repository data: {0}", req.error);
-                yield break;
-            }
+            Log("Fetching repository data");
 
-            // deserialize json data
-            _modules = JsonConvert.DeserializeObject<KtaneModuleResult>(req.downloadHandler.text).KtaneModules;
-            Log("Got {0} modules", _modules.Length);
+            // fetch json data
+            using (var req = UnityWebRequest.Get(string.Format("{0}/json/raw", RepositoryHost)))
+            {
+                yield return req.SendWebRequest();
+                if (req.isHttpError || req.isNetworkError)
+                {
+                    _isError = true;
+                    Log("Failed to get repository data: {0}", req.error);
+                    yield break;
+                }
+
+                // deserialize json data
+                _modules = JsonConvert.DeserializeObject<KtaneModuleResult>(req.downloadHandler.text).KtaneModules;
+                Log("Got {0} modules", _modules.Length);
+            }
+        }
+        finally
+        {
+            _fetchRepoDone = true;
         }
     }
 
     private IEnumerator FetchIconSprite()
     {
-        Log("Fetching iconsprite");
-
-        // fetch sprite
-        using (var req = UnityWebRequestTexture.GetTexture(string.Format("{0}/iconsprite", RepositoryHost)))
+        try
         {
-            yield return req.SendWebRequest();
-            if (req.isHttpError || req.isNetworkError)
-            {
-                _isError = true;
-                Log("Failed to get iconsprite: {0}", req.error);
-                yield break;
-            }
+            Log("Fetching iconsprite");
 
-            // get sprite
-            _iconSprite = DownloadHandlerTexture.GetContent(req);
-            Log("Got iconsprite ({0}x{1})", _iconSprite.width, _iconSprite.height);
+            // fetch sprite
+            using (var req = UnityWebRequestTexture.GetTexture(string.Format("{0}/iconsprite", RepositoryHost)))
+            {
+                yield return req.SendWebRequest();
+                if (req.isHttpError || req.isNetworkError)
+                {
+                    _isError = true;
+                    Log("Failed to get iconsprite: {0}", req.error);
+                    yield break;
+                }
+
+                // get sprite
+                _iconSprite = DownloadHandlerTexture.GetContent(req);
+                Log("Got iconsprite ({0}x{1})", _iconSprite.width, _iconSprite.height);
+            }
+        }
+        finally
+        {
+            _fetchIconSpriteDone = true;
         }
     }
 
@@ -82,8 +92,7 @@ public class IconFetch : MonoBehaviour
     {
         // make sure both routines are done
         // if they've already finished, callback is invoked instantly
-        yield return _fetchRoutineRepo;
-        yield return _fetchRoutineIconSprite;
+        yield return new WaitUntil(() => _fetchRepoDone && _fetchIconSpriteDone);
         callback.Invoke(_isError);
     }
 
